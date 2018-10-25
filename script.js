f8ef85b16fe77b6ebe3c40518a050cebcd637a5a
Jake K <jake.kopenhefer@gmail.com>
2018-10-25 16:46:43 -0600

    Dialog and function to create multiple player objects one at a time

--- script.js
+++ script.js
@@ -8,46 +8,22 @@ class Player {
         this.teeBox = teeBox;
     }
 }
+let players = [];
 
-let player1 = new Player("Jake Kopenhefer", "green");
-// player1.holeScore = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-
-
-
-// console.log($('.pOne1').text())
-
-// function strokeTotal(player){
-//     let total =0;
-//     let id;
-//     for(i=1; i<19; i++){
-//     id = "'." + player + i+"'"
-//     console.log(id);
-//     total = total + $(id).text();
-//     console.log(total);
-//     }
-//     return total;
-// }
-
-// $('.pOneTotal').html(strokeTotal);
-
-// strokeTotal('pOne');
-
-// function addPlayers(){
-//     for ....{
-//     new Player
-//     }
-// }
-
-function addplayers(){
-    let numbplayers = $('.numbinput').val();
-    for(let i = 0;i<numbplayers; i++){
-        $('.card').append(`<div>Player ${i+1}</div>`)
-    }
-    $('.modal').hide();
+function buildPlayer(){
+    players.push (new Player($('#player').val(),$('#teeBox').val()));
+    $('#player').val('');
+    $('#teeBox').val('')
 }
 
 
-//for the dialog box
+
+
+
+
+
+
+//for the MDL dialog box
 let dialog = document.querySelector('dialog');
 let showDialogButton = document.querySelector('#show-dialog');
 if (!dialog.showModal) {
