5637d5b40d92bb45d6279ffbff1a07935bfc630c
Jake K <jake.kopenhefer@gmail.com>
2018-10-26 21:59:25 -0600

    change dialog

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ class Player {
         this.totalScore = ()=>{return this.holeScore.reduce((a, b) => a + b, 0)};
         this.outScore = ()=>{return this.holeScore.slice(0,9).reduce((a,b) => a + b, 0)};
         this.inScore = () =>{return this.holeScore.slice(9, 18).reduce((a, b) => a + b, 0)};
-        this.teeBox = teeBox;
     }
 }
 let players = [];
 let idPromise = getCourseInfo();
-// let coursePromise = getCourseId();
+let coursePromise = getCourseId();
 
 function buildPlayer(){
     players.push (new Player($('#player').val(),$('#teeBox').val()));
@@ -19,8 +18,49 @@ function buildPlayer(){
     listPlayers();
 }
 
+function onCard(){
+    $('.cardList').empty();
+    for (i=0; i<players.length; i++){
+        $('.cardList').append(`
+            <tr class="player1">
+                <th class="">${players[i].name}</th>
+                <th class="p${i}-1">${players[i].holeScore[0]}</th>
+                <th class="p${i}-2">${players[i].holeScore[1]}</th>
+                <th class="p${i}-3">${players[i].holeScore[2]}</th>
+                <th class="p${i}-4">${players[i].holeScore[3]}</th>
+                <th class="p${i}-5">${players[i].holeScore[4]}</th>
+                <th class="p${i}-6">${players[i].holeScore[5]}</th>
+                <th class="p${i}-7">${players[i].holeScore[6]}</th>
+                <th class="p${i}-8">${players[i].holeScore[7]}</th>
+                <th class="p${i}-9">${players[i].holeScore[8]}</th>
+                <th class="p${i}-Out">${players[i].outScore()}</th>
+                <th class=""></th>
+                <th class="p${i}-10">${players[i].holeScore[9]}</th>
+                <th class="p${i}-11">${players[i].holeScore[10]}</th>
+                <th class="p${i}-12">${players[i].holeScore[11]}</th>
+                <th class="p${i}-13">${players[i].holeScore[12]}</th>
+                <th class="p${i}-14">${players[i].holeScore[13]}</th>
+                <th class="p${i}-15">${players[i].holeScore[14]}</th>
+                <th class="p${i}-16">${players[i].holeScore[15]}</th>
+                <th class="p${i}-17">${players[i].holeScore[16]}</th>
+                <th class="p${i}-18">${players[i].holeScore[17]}</th>
+                <th class="p${i}-In">${players[i].inScore()}</th>
+                <th class="p${i}-Total">${players[i].totalScore()}</th>
+            </tr>
+        `);
+    }
+}
+
 function listPlayers(){
-    $('.playerLst').child() // add the player name and tee to the card and the dialog
+    $('.playerList').empty();
+    for (i=0; i<players.length; i++){
+        $('.playerList').append(`<div class="names">${players[i].name}<i class="fas fa-trash-alt" onclick="dltPlayer(${i})"></i></div>`);
+    }
+}
+
+function dltPlayer(index){
+    players.splice(index,1);
+    listPlayers();
 }
 
 // calls
@@ -43,7 +83,7 @@ function getCourseId() {
     return new Promise((resolve, reject) => {
         $.ajax({
             url: "https://golf-courses-api.herokuapp.com/courses",
-            type: 'GET',
+            type: 'POST',
             success: response => {
                 resolve(response);
             },
