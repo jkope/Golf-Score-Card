91b1418cf7ff8a0bc714899b5ce975ff37587dbd
Jake K <jake.kopenhefer@gmail.com>
2018-10-29 19:56:43 -0600

    add jasmine tests - tdd

--- JS/scoring-service.js
+++ JS/scoring-service.js
@@ -1,3 +1,20 @@
 module.exports = class ScoringService {
-     
-}
+    calculateScoreofPar(par, scores){
+        return scores.map((score)=> (score ? score : 0)).reduce((a,b) => a+b, 0)-par;
+    }
+    calculateOutScore(par, scores){
+        let out = scores.slice(0, 9)
+        return out.map((score) => (score ? score : 0)).reduce((a, b) => a + b, 0) - par;
+    }
+    calculateInScore(par, scores) {
+        let instroke = scores.slice(9, 18)
+        return instroke.map((score) => (score ? score : 0)).reduce((a, b) => a + b, 0) - par;
+    }
+    totalofScore(scores) {
+        return scores.map((score) => (score ? score : 0)).reduce((a, b) => a + b, 0);
+    }
+
+};
+
+
+
--- spec/scoring-service.spec.js
+++ spec/scoring-service.spec.js
@@ -6,19 +6,19 @@ describe('Scoring-Service', ()=>{
         scoringService = new ScoringService();
     });
     describe('calculateScoreofPar',()=> {
-        it('calculates score reletive to par correctly when under par', () =>{
+        it('calculates score relative to par correctly when under par', () =>{
             let par = 72;
-            let playerScore = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+            let playerScore = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
             let scoreofPar =  scoringService.calculateScoreofPar(par,playerScore);
             expect(scoreofPar).toEqual(-18);
         });
-        it('return a number', () =>{
+        it('calculates score relative to par correctly when over par', () =>{
             let par = 72;
-            let playerScore = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+            let playerScore = Array(18).fill(5, 0, 18)
             let scoreofPar = scoringService.calculateScoreofPar(par, playerScore);
-            expect(scoreofPar).toEqual(-18); 
+            expect(scoreofPar).toEqual(18); 
         });
-        it('calculates score reletive to par correctly when over par', () =>{
+        it('returns a number', () =>{
             let par = 72;
             let playerScore = Array(18).fill(5,0,18);
             let scoreofPar = scoringService.calculateScoreofPar(par, playerScore);
@@ -31,13 +31,68 @@ describe('Scoring-Service', ()=>{
             expect(scoreofPar).toEqual(-72);
         });
     });
-    describe('calculateOutScore',()=>{
-
+    describe('calculateOutScore', ()=>{
+        it('calculates score relative to out-par correctly when under par', () => {
+            let par = 36;
+            let playerScore = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+            let scoreofPar = scoringService.calculateOutScore(par, playerScore);
+            expect(scoreofPar).toEqual(-9);
+        });
+        it('calculates score relative to out-par correctly when over par', () => {
+            let par = 36;
+            let playerScore = Array(18).fill(5, 0, 18);
+            let scoreofPar = scoringService.calculateOutScore(par, playerScore);
+            expect(scoreofPar).toEqual(9);
+        });
+        it('Returns a number', () => {
+            let par = 72;
+            let playerScore = Array(18).fill(5, 0, 18);
+            let scoreofPar = scoringService.calculateOutScore(par, playerScore);
+            expect(typeof scoreofPar).toEqual('number');
+        });
+        it('calculates even when no scores', () => {
+            let par = 36;
+            let playerScore = Array(18);
+            let scoreofPar = scoringService.calculateOutScore(par, playerScore);
+            expect(scoreofPar).toEqual(-36);
+        });
     });
     describe('calculateInScore',()=>{
-
+        it('calculates score relative to in-par correctly when under par', () => {
+            let par = 36;
+            let playerScore = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+            let scoreofPar = scoringService.calculateInScore(par, playerScore);
+            expect(scoreofPar).toEqual(-9);
+        });
+        it('calculates score relative to in-par correctly when over par', () => {
+            let par = 36;
+            let playerScore = Array(18).fill(5, 0, 18);
+            let scoreofPar = scoringService.calculateInScore(par, playerScore);
+            expect(scoreofPar).toEqual(9);
+        });
+        it('Returns a number', () => {
+            let par = 72;
+            let playerScore = Array(18).fill(5, 0, 18);
+            let scoreofPar = scoringService.calculateInScore(par, playerScore);
+            expect(typeof scoreofPar).toEqual('number');
+        });
+        it('calculates even when no scores', () => {
+            let par = 36;
+            let playerScore = Array(18);
+            let scoreofPar = scoringService.calculateInScore(par, playerScore);
+            expect(scoreofPar).toEqual(-36);
+        });
     });
     describe('calculateTotalScore',()=>{
-
+        it('Returns a number', () => {
+            let playerScore = Array(18).fill(5, 0, 18);
+            let totalScore = scoringService.totalofScore(playerScore);
+            expect(typeof totalScore).toEqual('number');
+        });
+        it('calculates even when no scores', () => {
+            let playerScore = Array(18);
+            let totalScore = scoringService.totalofScore(playerScore);
+            expect(totalScore).toEqual(0);
+        });
     });
 })
